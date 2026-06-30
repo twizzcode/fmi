04a2bec418da0853eb20fb49fed8c558cf53e707
twizzcode <twizzcode@gmail.com>
2026-07-01 05:42:56 +0700

    bismillah

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   allowedDevOrigins: ['lvh.me', 'admin.lvh.me', 'fmiunnes.com', 'admin.fmiunnes.com'],
   experimental: {
     serverActions: {
-      bodySizeLimit: '50mb',
+      bodySizeLimit: '100mb',
     },
   },
   images: {
